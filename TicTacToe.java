ca17fefa41643e9db4ef6549341b24ebc72c64e2
Davi Mello <davimello1994@gmail.com>
2020-06-08 19:04:48 -0400

    First commit

---
+++ TicTacToe.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main (String[] args) {
+        int [][] gameBoard = new int[3][3];
+
+        System.out.println("Welcome to Tic Tac Toe!\nPress any key to continue.");
+    }
+
+
+}
